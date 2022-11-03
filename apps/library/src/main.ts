94416ddd9fce9b238bd9b0fe744cea7db48f67b0
michael-nhat <nhathoang.haiphong@gmail.com>
2022-11-03 23:06:36 +0700

    yaml swagger 3 support

--- apps/library/src/main.ts
+++ apps/library/src/main.ts
@@ -3,9 +3,20 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+// import { swagerMiddleware, yamlwtf } from './swagger/swaggerMiddleware';
+import  * as swaggerUiExpress from 'swagger-ui-express';
+import *as  yaml from 'js-yaml';
+import * as path from 'path';
+import * as fs from 'fs';
+const swagger_path = path.resolve(__dirname, './assets/apidocs/swagger.yaml');
+const doc = yaml.load(fs.readFileSync(swagger_path, 'utf8'));
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use('/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(doc));
+  
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
