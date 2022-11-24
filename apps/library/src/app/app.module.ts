8357993e70be2d69a1274324d0f9aeebc0bf46f2
michael-nhat <nhathoang.haiphong@gmail.com>
2022-11-24 15:50:02 +0700

    feat: slonik , peer deps seem be defaul (for aware)

--- apps/library/src/app/app.module.ts
+++ apps/library/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { LendingUiRestModule } from '@library/lending/ui-rest';
 import { Module } from '@nestjs/common';
 import { AppCoreModule } from './app-core.module';
 import { DbModule } from './db.module';
+import { SlonikModule} from 'nestjs-slonik';
 
 @Module({
   imports: [
     AppCoreModule,
     CatalogueModule,
     LendingUiRestModule,
-    DbModule
+    DbModule,
+    SlonikModule.forRoot({
+      connectionUri: 'postgres://postgres@postgres@localhost/lib2',
+    })
   ],
 })
 export class AppModule {}
