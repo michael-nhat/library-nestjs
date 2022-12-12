1e47c7b4dd7503c3b9e2b66c33e3fef5a8eafdba
hoang van nhat <nhat.hoangvan@ncc.asia>
2022-12-12 19:39:20 +0700

    feat: config db

--- libs/catalogue/src/lib/catalogue.module.ts
+++ libs/catalogue/src/lib/catalogue.module.ts
@@ -1,8 +1,30 @@
+import { ObjectionModule } from '@willsoto/nestjs-objection';
 import { Module } from '@nestjs/common';
 import { CatalogueController } from './catalogue.controller';
 import { CatalogueService } from './catalogue.service';
+import { SlonikModule } from 'nestjs-slonik';
 
 @Module({
+  imports: [
+    SlonikModule.forRoot({
+      connectionUri: 'postgres://postgres:postgres@localhost/lib2',
+    }),
+    ObjectionModule.register({
+      config: {
+        client: 'postgres',
+        useNullAsDefault: true,
+        connection: {
+          database: process.env.DB_NNAME,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          host: process.env.DB_HOST,
+          port: 5432,
+          ssl: false,
+        },
+      },
+    }),
+    ObjectionModule.forFeature([]),
+  ],
   controllers: [CatalogueController],
   providers: [CatalogueService],
   exports: [CatalogueService],
