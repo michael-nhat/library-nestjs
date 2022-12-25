57262f2bb600c521062a17a78865b41f21e81467
hoang van nhat <nhat.hoangvan@ncc.asia>
2022-12-25 21:25:06 +0700

    feat: init user api

--- libs/ambass/src/lib/ambass.module.ts
+++ libs/ambass/src/lib/ambass.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AmbassController } from './ambass.controller';
 import { AmbassService } from './ambass.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [AmbassController],
   providers: [AmbassService],
   exports: [AmbassService],
+  imports: [UserModule],
 })
 export class AmbassModule {}
