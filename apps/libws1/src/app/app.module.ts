1a53d2297dd81d070050cebeb8fc78001becc251
hoang van nhat <nhat.hoangvan@ncc.asia>
2022-12-25 21:31:40 +0700

    api: add user endpoint

--- apps/libws1/src/app/app.module.ts
+++ apps/libws1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatalogueModule } from '@libws/catalogue';
+import { UserModule } from '@lib/ambass';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    CatalogueModule
+    CatalogueModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- libs/ambass/src/index.ts
+++ libs/ambass/src/index.ts
@@ -1,3 +1,6 @@
 export * from './lib/ambass.controller';
 export * from './lib/ambass.service';
 export * from './lib/ambass.module';
+export * from './lib/user/user.controller';
+export * from './lib/user/user.service';
+export * from './lib/user/user.module';
