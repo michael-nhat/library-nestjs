de471b9e59893c522a9d597c7d2ba7659bebdcfa
hoang van nhat <nhat.hoangvan@ncc.asia>
2022-12-25 21:49:58 +0700

    auth module

--- libs/ambass/src/index.ts
+++ libs/ambass/src/index.ts
@@ -1,6 +1,11 @@
 export * from './lib/ambass.controller';
 export * from './lib/ambass.service';
 export * from './lib/ambass.module';
+
 export * from './lib/user/user.controller';
 export * from './lib/user/user.service';
 export * from './lib/user/user.module';
+
+export * from './lib/auth/auth.controller';
+export * from './lib/auth/auth.service';
+export * from './lib/auth/auth.module';
--- libs/ambass/src/lib/ambass.module.ts
+++ libs/ambass/src/lib/ambass.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AmbassController } from './ambass.controller';
 import { AmbassService } from './ambass.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [AmbassController],
   providers: [AmbassService],
   exports: [AmbassService],
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
 })
 export class AmbassModule {}
